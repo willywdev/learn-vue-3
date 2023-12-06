bdeabb4374aa6df4c0f130a9e31d09684f164db0
willywdev <werwein.dev@gmail.com>
2023-12-06 12:34:00 +0100

    Update Vue course goals HTML and JavaScript files

--- 2-vue-course-goals/main.js
+++ 2-vue-course-goals/main.js
@@ -1,1 +1,9 @@
-Vue.createApp({}).mount("#user-goal");
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoal: "Finish the course and learn Vue!",
+    };
+  },
+});
+
+app.mount("#user-goal");
